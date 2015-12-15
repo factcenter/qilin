799fd5f55a60ac93beb78b690d6d39f85014a0d3
Tal Moran <talm@factcenter.org>
2015-12-15 02:01:06 +0200

    Github javadocs

--- src/main/java/org/factcenter/qilin/comm/Sendable.java
+++
@@ -1,9 +0,0 @@
-package org.factcenter.qilin.comm;
-
-import java.io.IOException;
-
-public interface Sendable {
-	public void writeTo(SendableOutput out) throws IOException;
-	
-	public void readFrom(SendableInput in) throws IOException;
-}
